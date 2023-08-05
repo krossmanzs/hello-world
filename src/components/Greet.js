3ef393daf34c79de4d316120627eb82c045d628a
Cornelius Linux <radi.adi78@gmail.com>
2023-08-05 15:21:05 +0700

    Functional components

---
+++ src/components/Greet.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// normal function syntax
+// function Greet() {
+//     return <h1>Halo dunia</h1>
+// }
+
+// arrow function syntax
+// const Greet = () => <h1>Halo dunia</h1>
+
+// name export
+export const Greet = () => <h1>Halo dunia</h1>
+
+// export Greet component as default component of this file
+// export default Greet
