9bdf59bda50f834217d7d71bd3d0d15b7edf5551
Cornelius Linux <radi.adi78@gmail.com>
2023-08-05 15:29:13 +0700

    Class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Greet } from './components/Greet'; // must be use curly braces if name export
+import { Welcome } from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Greet/>
+      <Welcome/>
     </div>
   );
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,29 @@
+// need to include 2 imports if we need to create class component
+import React, { Component } from 'react'
+
+/*
+    Functional vs Class Components
+    
+    Functional:
+    - Simple Functions
+    - Use Func components as much as possible
+    - Absence of 'this' keyword
+    - Solution without using state
+    - Mainly responsible for the UI
+    - Stateless/ Dumb/ Presentational
+
+    Class Components:
+    - More feature rich
+    - Maintain their own private data - state
+    - Complex UI logic
+    - Provide lifecycle hooks
+    - Stateful/ Smart/ Container
+*/
+
+class Welcome extends Component {
+    render() {
+        return <h1>This is Class Component</h1> // JSX
+    }
+}
+
+export default Welcome
